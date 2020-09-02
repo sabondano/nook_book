d199ff5e13222be517672eaedd8c676ad1bb75ca
Sebastian Abondano <sabondano1@gmail.com>
2020-09-02 15:25:25 -0400

    Step 13

---
+++ config/releases.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :nook_book, NookBookWeb.Endpoint,
+  server: true,
+  http: [port: 4000],
+  url: [host: "localhost"],
+  secret_keybase: "dvSyRz8WDLW1vst8s0OUeCapZ0qJM5grZ+2L8NibWLCfxDypIHaUu0of8g+nclzx"
